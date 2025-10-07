b472c2061ae5739d6d7f5ecd90c85eb0b7c709f7
thopterulu <guillaume.kergueris@gmail.com>
2025-10-07 21:49:21 +0200

    fix wallpaper script

--- configuration.nix
+++ configuration.nix
@@ -52,7 +52,7 @@
    sleep 1
    xrandr --output DP-2 --primary --mode 2560x1440 --rate 120 --pos 0x0 \
          --output HDMI-1-1 --mode 1920x1080 --rate 60 --pos 2560x0 &
-   /home/thopter/nixos-config/wallpaper-changer.sh &
+   ${pkgs.bash}/bin/bash /home/thopter/nixos-config/wallpaper-changer.sh &
    xset r rate 200 35 &
     '';
   };
--- wallpaper-changer.sh
+++ wallpaper-changer.sh
@@ -5,7 +5,7 @@ WALLPAPER_DIR="$HOME/backgrounds"
 while true; do
     if [ -d "$WALLPAPER_DIR" ] && [ "$(ls -A "$WALLPAPER_DIR")" ]; then
         # Get random wallpaper
-        WALLPAPER=$(find "$WALLPAPER_DIR" -type f \( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" -o -iname "*.bmp" \) | shuf -n 1)
+        WALLPAPER=$(find "$WALLPAPER_DIR" -type f \( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" -o -iname "*.bmp" -o -iname "*.webp" \) | shuf -n 1)
         
         if [ -n "$WALLPAPER" ]; then
             xwallpaper --output DP-2 --zoom "$WALLPAPER" \
