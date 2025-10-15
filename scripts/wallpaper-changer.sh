6aa146451ca969e04c6e9eee393762fd046887b3
thopterulu <guillaume.kergueris@gmail.com>
2025-10-15 15:11:18 +0200

    wallpaper adventure chaptger 80

--- scripts/wallpaper-changer.sh
+++ scripts/wallpaper-changer.sh
@@ -8,8 +8,10 @@ while true; do
         WALLPAPER=$(find "$WALLPAPER_DIR" -type f \( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" -o -iname "*.bmp" -o -iname "*.webp" \) | shuf -n 1)
         
         if [ -n "$WALLPAPER" ]; then
-            xwallpaper --output DP-2 --zoom "$WALLPAPER" \
-                      --output HDMI-1-1 --zoom "$WALLPAPER"
+            # Get all connected displays and set wallpaper on each
+            xrandr --query | grep " connected" | cut -d' ' -f1 | while read output; do
+                xwallpaper --output "$output" --zoom "$WALLPAPER"
+            done
         fi
     fi
     
