05c058b2509487d873e75773b26e01a087c6dc0d
thopterulu <guillaume.kergueris@gmail.com>
2025-10-23 22:05:03 +0200

    oopsy daisies

--- configs/configuration-desktop.nix
+++ configs/configuration-desktop.nix
@@ -25,8 +25,8 @@
   # Desktop-specific display configuration
   services.xserver.displayManager.sessionCommands = ''
    sleep 1
-   xrandr --output DP-1-2 --primary --mode 2560x1440 --rate 120 --pos 0x0 \
-         --output HDMI-1 --mode 1920x1080 --rate 60 --pos 2560x0 &
+   xrandr --output DP-2 --primary --mode 2560x1440 --rate 120 --pos 0x0 \
+         --output HDMI-1-1 --mode 1920x1080 --rate 60 --pos 2560x0 &
 
    xset r rate 200 35 &
   '';
