f2a323d4b243cba04c1889153624f97df1376c77
thopterulu <guillaume.kergueris@gmail.com>
2025-11-08 00:46:20 +0100

    erased

--- gaming-conf.nix
+++ gaming-conf.nix
@@ -1,17 +1,5 @@
 { config, lib, pkgs, ... }:
 
 {
-  # Gaming-specific firewall configuration
-  networking.firewall = {
-    enable = true;
-    allowedTCPPorts = [ 27015 27016 27017 27018 27019 27020 27025 27030 ];
-    allowedUDPPorts = [ 27000 27001 27015 27031 27036 3478 4379 4380 ];
-    allowedUDPPortRanges = [
-      { from = 27000; to = 27100; }
-      { from = 3478; to = 4380; }
-    ];
-    allowedTCPPortRanges = [
-      { from = 27015; to = 27030; }
-    ];
-  };
+
 }
