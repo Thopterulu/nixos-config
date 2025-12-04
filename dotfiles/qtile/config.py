0004a470f271738ae4da2c94dc0e998abf1e3b44
thopterulu <guillaume.kergueris@gmail.com>
2025-12-04 20:14:15 +0100

    try things

--- dotfiles/qtile/config.py
+++ dotfiles/qtile/config.py
@@ -193,12 +193,14 @@ keys = [
     Key([mod, "shift"], "k", lazy.layout.shuffle_up(), desc="Move window up"),
     # Grow windows. If current window is on the edge of screen and direction
     # will be to screen edge - window would shrink.
-    Key([mod, "control"], "h", lazy.layout.grow_left(), desc="Grow window to the left"),
-    Key(
-        [mod, "control"], "l", lazy.layout.grow_right(), desc="Grow window to the right"
-    ),
-    Key([mod, "control"], "j", lazy.layout.grow_down(), desc="Grow window down"),
-    Key([mod, "control"], "k", lazy.layout.grow_up(), desc="Grow window up"),
+    # Key([mod, "control"], "h", lazy.layout.grow_left(), desc="Grow window to the left"),
+    # Key(
+    #     [mod, "control"], "l", lazy.layout.grow_right(), desc="Grow window to the right"
+    # ),
+    # Key([mod, "control"], "j", lazy.layout.grow_down(), desc="Grow window down"),
+    # Key([mod, "control"], "k", lazy.layout.grow_up(), desc="Grow window up"),
+    Key([mod], "right", lazy.layout.next(), desc="Show next window"),
+    Key([mod], "left", lazy.layout.previous(), desc="show previous window"),
     Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
     # MonadTall specific keybindings
     Key([mod], "i", lazy.layout.grow(), desc="Grow main window"),
