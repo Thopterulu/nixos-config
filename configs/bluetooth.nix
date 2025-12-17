09c07d780b014784a514586659c13ea1237f2f06
thopterulu <guillaume.kergueris@gmail.com>
2025-12-17 17:53:37 +0100

    chagned ble

--- configs/bluetooth.nix
+++ configs/bluetooth.nix
@@ -5,7 +5,7 @@
     enable = true;
     powerOnBoot = true;
     settings.General = {
-      experimental = true; # show battery
+      # experimental = true; # show battery
 
       # https://www.reddit.com/r/NixOS/comments/1ch5d2p/comment/lkbabax/
       # for pairing bluetooth controller
@@ -17,13 +17,13 @@
   };
   services.blueman.enable = true;
 
-  hardware.xpadneo.enable = true; # Enable the xpadneo driver for Xbox One wireless controllers
+  # hardware.xpadneo.enable = true; # Enable the xpadneo driver for Xbox One wireless controllers
 
-  boot = {
-    extraModulePackages = with config.boot.kernelPackages; [ xpadneo ];
-    extraModprobeConfig = ''
-      options bluetooth disable_ertm=Y
-    '';
+  # boot = {
+    # extraModulePackages = with config.boot.kernelPackages; [ xpadneo ];
+    # extraModprobeConfig = ''
+    #   options bluetooth disable_ertm=Y
+    # '';
     # connect xbox controller
-  };
+  # };
 }
