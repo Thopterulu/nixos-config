da97a8001968f9312914525adb0b13662c3e93c4
thopterulu <guillaume.kergueris@gmail.com>
2026-03-29 23:26:27 +0200

    wdisplays

--- configs/hyprland.nix
+++ configs/hyprland.nix
@@ -51,6 +51,7 @@
     slurp                # Region selector for Wayland
     grimblast            # Screenshot wrapper for Hyprland
     swappy               # Screenshot annotation tool
+    wdisplays            # GUI display configuration tool
     jq                   # JSON processor for waybar scripts
     hyprpicker           # Color picker for Wayland
     hyprlock             # Screen locker for Hyprland
