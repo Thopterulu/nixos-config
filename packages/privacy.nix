6e52fd355cd2a35a5951a4056a16ce660af37fc9
thopterulu <guillaume.kergueris@gmail.com>
2026-09-10 19:43:19 +0200

    pv

--- packages/privacy.nix
+++ packages/privacy.nix
@@ -2,6 +2,7 @@
 
 {
   home.packages = with pkgs; [
+    mullvad               
     mullvad-vpn          # VPN client
     tor                  # Anonymous network relay
     tor-browser          # Privacy-focused web browser
