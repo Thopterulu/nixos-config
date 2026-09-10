aa941b7427b8f3668c4c3f436db0bd72cc83a428
thopterulu <guillaume.kergueris@gmail.com>
2026-09-10 19:39:31 +0200

    ✨feature: added privacy tools

--- packages/entertainment.nix
+++ packages/entertainment.nix
@@ -5,8 +5,6 @@
     discord              # Voice/text chat
     mixxx                # DJ mixing software
     scdl                 # SoundCloud track downloader
-    tor                  # Anonymous network relay
-    tor-browser          # Privacy-focused web browser
     davinci-resolve      # Professional video editor
     shotcut              # Open-source video editor
     kdePackages.kdenlive # KDE video editor
---
+++ packages/privacy.nix
@@ -0,0 +1,10 @@
+{ pkgs, ... }:
+
+{
+  home.packages = with pkgs; [
+    mullvad-vpn          # VPN client
+    tor                  # Anonymous network relay
+    tor-browser          # Privacy-focused web browser
+    proton-vpn           # VPN client
+  ];
+}
